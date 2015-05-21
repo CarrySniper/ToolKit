a807732aae2fc8feb4b43cecbcd0a37d5c2b7b21
CK_chan <692771080@qq.com>
2015-05-21 15:59:05 +0800

    Initial Commit

---
+++ ToolKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToolKit
+//
+//  Created by CK_chan on 15/5/21.
+//  Copyright (c) 2015年 CK_chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ToolKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToolKit
+//
+//  Created by CK_chan on 15/5/21.
+//  Copyright (c) 2015年 CK_chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
